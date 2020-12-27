a72fe2fad1638d5b3cc198f882d256ff862a709f
AlexChigur <And1933@yandex.ru>
2020-12-27 23:11:47 +0300

    ''

---
+++ components/ContentItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="content">
+    <div class="post">
+
+    </div>
+    <button>Add comment</button>
+  </div>
+
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
